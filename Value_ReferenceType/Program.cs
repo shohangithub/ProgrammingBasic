0a61885833bfbe90ed468b53f2ca733f38f6484c
Rafiul Islam Shohan <ris.shohan@gmail.com>
2021-02-09 17:44:10 +0600

    add value and reference type

---
+++ Value_ReferenceType/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Value_ReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueType vt;
+            vt = new ValueType();
+
+            ReferenceType rt;
+            rt = new ReferenceType();
+            
+        }
+
+
+        public class ValueType
+        {
+            public ValueType()
+            {
+                int i = 100;
+
+                Console.WriteLine(i);
+
+                ChangeValue(i);
+
+                Console.WriteLine(i);
+            }
+            static void ChangeValue(int x)
+            {
+                x = 200;
+
+                Console.WriteLine(x);
+            }
+            
+        }
+
+        public class ReferenceType
+        {
+            public ReferenceType()
+            {
+                Student std1;
+                std1 = new Student();
+                std1.StudentName = "Bill";
+                Console.WriteLine($"Old Value : {std1.StudentName}");
+                ChangeReferenceType(std1);
+                Console.WriteLine($"New Value : {std1.StudentName}");
+            }
+            static void ChangeReferenceType(Student std2)
+            {
+                std2.StudentName = "Steve";
+            }
+        }
+
+
+        private class Student
+        {
+            public string StudentName { get; set; }
+        }
+
+    }
+}
