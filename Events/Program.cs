0b889be75c8e8bc8f6e64c196850f73abad86cdb
Rafiul Islam Shohan <ris.shohan@gmail.com>
2021-11-01 09:29:34 +0600

    init event

---
+++ Events/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Events
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted(object sender, ProcessEventArgs e)
+        {
+            Console.WriteLine("Process " + (e.IsSuccessful ? "Completed Successfully" : "failed"));
+            Console.WriteLine("Completion Time: " + e.CompletionTime);
+        }
+    }
+
+    public class ProcessBusinessLogic
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler<ProcessEventArgs> ProcessCompleted;
+
+        public void StartProcess()
+        {
+            var data = new ProcessEventArgs();
+            try
+            {
+                Console.WriteLine("Process Started!");
+                data.StartTime = DateTime.Now;
+                
+                //Delay 5 seconds
+                Thread.Sleep(5000);
+                
+                data.IsSuccessful = true;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+            catch
+            {
+                data.IsSuccessful = false;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+        }
+
+        protected virtual void OnProcessCompleted(ProcessEventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+
+    public class ProcessEventArgs : EventArgs
+    {
+        public DateTime StartTime { get; set; }
+        public bool IsSuccessful { get; set; }
+        public DateTime CompletionTime { get; set; }
+    }
+}
