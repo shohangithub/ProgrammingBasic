510035e0e2b98eb8eae20e767e1a3c2f2f1865e9
Rafiul Islam Shohan <shohan@nextit.com.bd>
2021-11-23 15:10:27 +0600

    init generic in c#

---
+++ Generic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int is the type argument
+            GenericList<int> list = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.AddHead(x);
+            }
+            foreach (int i in list)
+            {
+                System.Console.Write(i + " ");
+            }
+            System.Console.WriteLine("\nDone");
+        }
+    }
+}
